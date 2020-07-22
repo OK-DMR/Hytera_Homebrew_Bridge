602140165ca54f9d9934568445721f2cbdf33530
Marek Sebera <marek.sebera@gmail.com>
2020-07-22 18:14:35 +0200

    DTP data transmit protocol implemented, currently incorrect size of last slice expected

---
+++ kaitai/data_transmit_protocol.ksy
@@ -0,0 +1,107 @@
+meta:
+  id: data_transmit_protocol
+  imports:
+    - radio_ip
+enums:
+  service_types:
+    0xA0: data_transmit_protocol
+  service_specific_types:
+    0x01: dtp_request
+    0x11: dtp_answer
+    0x02: data_slice_trasmit
+    0x12: data_slice_answer
+    0x03: last_data_slice
+    0x13: last_data_slice_answer
+  results:
+    0x00: success
+    0x01: failure
+types:
+  dtp_request:
+    seq:
+      - id: destination_ip
+        type: radio_ip
+      - id: source_ip
+        type: radio_ip
+      - id: file_size
+        type: u2be
+        doc: size in bytes
+      - id: file_name
+        type: str
+        terminator: 0
+        encoding: UTF16-LE
+        doc: maximum of 256 bytes including file extension, if longer, recipient should refuse the transmission
+  dtp_answer:
+    seq:
+      - id: destination_ip
+        type: radio_ip
+      - id: source_ip
+        type: radio_ip
+      - id: result
+        type: u1
+        enum: results
+  data_slice_transmit:
+    seq:
+      - id: destination_ip
+        type: radio_ip
+      - id: source_ip
+        type: radio_ip
+      - id: block_number
+        type: u2be
+        doc: sequence number in transfer, starting with 1
+      - id: file_data
+        size: 448
+        doc: 448 bytes slice of transmitted file, or shorter if current slice is the last one
+  data_slice_answer:
+    seq:
+      - id: destination_ip
+        type: radio_ip
+      - id: source_ip
+        type: radio_ip
+      - id: block_number
+        type: u2be
+        doc: sequence number in transfer, same as data_slice_transmit.block_number the response is for
+      - id: result
+        type: u1
+        enum: results
+        doc: result of transmit from receiving end
+  last_data_slice:
+    doc: sent by transmit source, requires answer from the destination
+    seq:
+      - id: destination_ip
+        type: radio_ip
+        doc: transmit recipient ip
+      - id: source_ip
+        type: radio_ip
+        doc: transmit sender ip
+  last_data_slice_answer:
+    seq:
+      - id: destination_ip
+        type: radio_ip
+        doc: transmit sender ip
+      - id: source_ip
+        type: radio_ip
+        doc: transmit recipient ip
+      - id: result
+        type: u1
+        enum: results
+seq:
+  - id: service_type
+    type: u1
+    enum: service_types
+    doc: should be always 0xA0 Data Transmit Protocol
+  - id: service_specific_type
+    type: u1
+    enum: service_specific_types
+  - id: message_length
+    type: u2be
+    doc: length of the message from next field to the end of DTP message
+  - id: data
+    type:
+      switch-on: service_specific_type
+      cases:
+        service_specific_types::dtp_request: dtp_request
+        service_specific_types::dtp_answer: dtp_answer
+        service_specific_types::data_slice_transmit: data_slice_transmit
+        service_specific_types::data_slice_answer: data_slice_answer
+        service_specific_types::last_data_slice: last_data_slice
+        service_specific_types::last_data_slice_answer: last_data_slice_answer
---
+++ kaitai/data_transmit_protocol.py
@@ -0,0 +1,143 @@
+# This is a generated file! Please edit source .ksy file and use kaitai-struct-compiler to rebuild
+
+from pkg_resources import parse_version
+from kaitaistruct import __version__ as ks_version, KaitaiStruct, KaitaiStream, BytesIO
+from enum import Enum
+
+
+if parse_version(ks_version) < parse_version("0.7"):
+    raise Exception(
+        "Incompatible Kaitai Struct Python API: 0.7 or later is required, but you have %s"
+        % (ks_version)
+    )
+
+from kaitai import radio_ip
+
+
+class DataTransmitProtocol(KaitaiStruct):
+    class ServiceTypes(Enum):
+        data_transmit_protocol = 160
+
+    class ServiceSpecificTypes(Enum):
+        dtp_request = 1
+        data_slice_trasmit = 2
+        last_data_slice = 3
+        dtp_answer = 17
+        data_slice_answer = 18
+        last_data_slice_answer = 19
+
+    class Results(Enum):
+        success = 0
+        failure = 1
+
+    def __init__(self, _io, _parent=None, _root=None):
+        self._io = _io
+        self._parent = _parent
+        self._root = _root if _root else self
+        self._read()
+
+    def _read(self):
+        self.service_type = KaitaiStream.resolve_enum(
+            self._root.ServiceTypes, self._io.read_u1()
+        )
+        self.service_specific_type = KaitaiStream.resolve_enum(
+            self._root.ServiceSpecificTypes, self._io.read_u1()
+        )
+        self.message_length = self._io.read_u2be()
+        _on = self.service_specific_type
+        if _on == self._root.ServiceSpecificTypes.dtp_request:
+            self.data = self._root.DtpRequest(self._io, self, self._root)
+        elif _on == self._root.ServiceSpecificTypes.data_slice_transmit:
+            self.data = self._root.DataSliceTransmit(self._io, self, self._root)
+        elif _on == self._root.ServiceSpecificTypes.data_slice_answer:
+            self.data = self._root.DataSliceAnswer(self._io, self, self._root)
+        elif _on == self._root.ServiceSpecificTypes.last_data_slice_answer:
+            self.data = self._root.LastDataSliceAnswer(self._io, self, self._root)
+        elif _on == self._root.ServiceSpecificTypes.dtp_answer:
+            self.data = self._root.DtpAnswer(self._io, self, self._root)
+        elif _on == self._root.ServiceSpecificTypes.last_data_slice:
+            self.data = self._root.LastDataSlice(self._io, self, self._root)
+
+    class LastDataSlice(KaitaiStruct):
+        """sent by transmit source, requires answer from the destination."""
+
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+
+    class DtpRequest(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+            self.file_size = self._io.read_u2be()
+            self.file_name = (self._io.read_bytes_term(0, False, True, True)).decode(
+                u"UTF16-LE"
+            )
+
+    class DataSliceAnswer(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+            self.block_number = self._io.read_u2be()
+            self.result = KaitaiStream.resolve_enum(
+                self._root.Results, self._io.read_u1()
+            )
+
+    class DataSliceTransmit(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+            self.block_number = self._io.read_u2be()
+            self.file_data = self._io.read_bytes(448)
+
+    class LastDataSliceAnswer(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+            self.result = KaitaiStream.resolve_enum(
+                self._root.Results, self._io.read_u1()
+            )
+
+    class DtpAnswer(KaitaiStruct):
+        def __init__(self, _io, _parent=None, _root=None):
+            self._io = _io
+            self._parent = _parent
+            self._root = _root if _root else self
+            self._read()
+
+        def _read(self):
+            self.destination_ip = radio_ip.RadioIp(self._io)
+            self.source_ip = radio_ip.RadioIp(self._io)
+            self.result = KaitaiStream.resolve_enum(
+                self._root.Results, self._io.read_u1()
+            )
