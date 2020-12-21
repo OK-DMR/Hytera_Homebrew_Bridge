42076504cfc2d2bce3674c97bb8acbda5daded2f
Marek Sebera <marek.sebera@gmail.com>
2020-12-21 23:13:32 +0100

    Release 2020.1 readme updated and setup script

--- README.md
+++ README.md
@@ -4,9 +4,36 @@
 ![Code Style: Python Black](https://img.shields.io/badge/code%20style-black-000000.svg)
 ![License](https://img.shields.io/github/license/smarek/Hytera_Homebrew_Bridge)
 
-**This project is currently non-functional and under heavy development**
 
-**Once the project is working and ready for alpha/beta testing, i'll update this README**
+**21.12.2020: We've entered alpha testing phase, see instructions below, on how to install and test**
+
+----
+
+This software will interconnect your Hytera repeater (tested RD625 and RD985) with MMDVM server (HBlink, DMRGateway, ...)
+
+----
+## Simple install:
+```shell
+$ python3 -m pip install hytera-homebrew-bridge
+$ curl "https://raw.githubusercontent.com/OK-DMR/Hytera_Homebrew_Bridge/master/settings.ini.default" -o settings.ini
+# Now edit settings.ini
+# Hytera: You must provide at least IPSC IP + ports (P2P, DMR and RDAC)
+# MMDVM: You must provide at least local_ip, master_ip, port and password
+$ hytera-homebrew-bridge.py settings.ini
+```
+
+----
+## Developer install:
+
+```shell
+$ git clone https://github.com/OK-DMR/Hytera_Homebrew_Bridge.git
+$ cd Hytera_Homebrew_Bridge
+$ cp settings.ini.default settings.ini
+$ apt-get install libsnmp-dev snmp-mibs-downloader gcc python-dev
+$ pip3 install -r requirements.txt --user --upgrade
+$ pip3 install -r requirements.development.txt --user --upgrade
+$ python3 bin/hytera-homebrew-bridge.py settings.ini
+```
 
 ----
 
--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
+#!/usr/bin/env python3
+import pathlib
+
 from setuptools import setup
+
+HERE = pathlib.Path(__file__).parent
+
+# The text of the README file
+README = (HERE / "README.md").read_text()
 
 setup(
     name="hytera-homebrew-bridge",
     description="Connect Hytera IPSC repeater to MMDVM system such as HBlink, DMRGateway",
+    long_description=README,
+    long_description_content_type="text/markdown",
     url="https://github.com/OK-DMR/Hytera_Homebrew_Bridge",
     author="Marek Sebera",
     author_email="marek.sebera@gmail.com",
@@ -16,7 +26,8 @@ setup(
     ],
     zip_safe=False,
     scripts=["bin/hytera-homebrew-bridge.py"],
-    keywords="mmdvm dmr hytera repeater",
+    keywords="mmdvm dmr hytera repeater ham radio repeater",
+    python_requires="~=3.7",
     install_requires=[
         "configparser>=5.0.0",
         "kaitaistruct>=0.9",
@@ -24,5 +35,18 @@ setup(
         "asyncio>=3.4.3",
         "dmr_utils3>=0.1.29",
         "bitarray>=1.6.1",
+    ],
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "License :: OSI Approved :: GNU Affero General Public License v3 or later (AGPLv3+)",
+        "Environment :: Console",
+        "Topic :: Communications :: Ham Radio",
+        "Operating System :: POSIX :: Linux",
+        "Typing :: Typed",
+        "Framework :: Pytest",
+        "Framework :: Flake8",
+        "Intended Audience :: Telecommunications Industry",
+        "Natural Language :: English",
+        "Programming Language :: Python :: 3.7",
     ],
 )
