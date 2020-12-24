aaf4efad3276060ea2e5856751e65bf09d0b6ba0
Marek Sebera <marek.sebera@gmail.com>
2020-12-24 11:40:04 +0100

    switch snmp library to support systems without net-snmp, updated readme for windows and added explicit minimal settings.ini example

--- README.md
+++ README.md
@@ -9,29 +9,59 @@
 
 ----
 
-This software will interconnect your Hytera repeater (tested RD625 and RD985) with MMDVM server (HBlink, DMRGateway, ...)
+This software will interconnect your Hytera repeater (tested RD625 and RD985) with MMDVM server (HBlink, DMRGateway, ...)  
+It does not require running under root/admin user, if you bind to ports over 1024
 
 ----
 ## Simple install:
 ```shell
-$ python3 -m pip install hytera-homebrew-bridge
-$ curl "https://raw.githubusercontent.com/OK-DMR/Hytera_Homebrew_Bridge/master/settings.ini.default" -o settings.ini
+# You need to have Python3 installed, at least version 3.7
+$ python3 -m pip install pip wheel setuptools --upgrade
+$ python3 -m pip install hytera-homebrew-bridge --upgrade
+# download config file
+$ curl "https://raw.githubusercontent.com/OK-DMR/Hytera_Homebrew_Bridge/master/settings.ini.minimal.default" -o settings.ini
 # Now edit settings.ini
 # Hytera: You must provide at least IPSC IP + ports (P2P, DMR and RDAC)
 # MMDVM: You must provide at least local_ip, master_ip, port and password
+# See "settings.ini.minimal.default" for required params / minimal configuration
 $ hytera-homebrew-bridge.py settings.ini
 ```
 
 ----
+## Install on Windows 
+
+To get software running on Windows, you need to install appropriate Python 3.7+ package (depending on your Windows version),
+and you need to install dependencies (MSVC++ 14) required to build **bitarray** dependency
+
+- Microsoft Visual C++ Build Tools v14.0
+  - Use [visualcppbuildtools_full.exe](https://go.microsoft.com/fwlink/?LinkId=691126) and install with default configuration
+  
+Then you should be able to use **Simple install**
+```shell
+# From standard Windows Command Line (cmd.exe) 
+$ python -m pip install pip wheel setuptools --upgrade
+$ python -m pip install hytera-homebrew-bridge --upgrade
+# Download settings from the project, settings.ini.minimal.default or settings.ini.default and modify it
+$ 
+```
+
+
+----
 ## Developer install:
 
+Run the software without installing to Python packages, so you can edit code and run the edits
+
 ```shell
+# Optionally uninstall the version installed in system
+# python3
 $ git clone https://github.com/OK-DMR/Hytera_Homebrew_Bridge.git
 $ cd Hytera_Homebrew_Bridge
+# You can use either settings.ini.default (all configuration params) or settings.ini.minimal.default (only required params)
 $ cp settings.ini.default settings.ini
 $ apt-get install libsnmp-dev snmp-mibs-downloader gcc python-dev
-$ pip3 install -r requirements.txt --user --upgrade
-$ pip3 install -r requirements.development.txt --user --upgrade
+$ python3 -m pip install -r requirements.txt --user --upgrade
+# Dependencies to run tests (pytest, pcapng parsing, ...)
+$ python3 -m pip install -r requirements.development.txt --user --upgrade
 $ python3 bin/hytera-homebrew-bridge.py settings.ini
 ```
 
--- hytera_homebrew_bridge/lib/snmp.py
+++ hytera_homebrew_bridge/lib/snmp.py
@@ -1,18 +1,23 @@
 #!/usr/bin/env python3
+import sys
 
-from easysnmp import snmp_walk, SNMPVariable
+from puresnmp import get
+from puresnmp.const import Version
 
 from hytera_homebrew_bridge.lib.settings import BridgeSettings
 from hytera_homebrew_bridge.lib.utils import octet_string_to_utf8
 
 
 class SNMP:
+    # in milli-volts (V * 1000)
     OID_PSU_VOLTAGE: str = "iso.3.6.1.4.1.40297.1.2.1.2.1.0"
+    # in milli-celsius (C * 1000)
     OID_PA_TEMPERATURE: str = "iso.3.6.1.4.1.40297.1.2.1.2.2.0"
+    # voltage ratio on the TX in dB
     OID_VSWR: str = "iso.3.6.1.4.1.40297.1.2.1.2.4.0"
-    # Forward power
+    # Forward power in milli-watt
     OID_TX_FWD_POWER: str = "iso.3.6.1.4.1.40297.1.2.1.2.5.0"
-    # Reflected power
+    # Reflected power in milli-watt
     OID_TX_REF_POWER: str = "iso.3.6.1.4.1.40297.1.2.1.2.6.0"
     OID_RSSI_TS1: str = "iso.3.6.1.4.1.40297.1.2.1.2.9.0"
     OID_RSSI_TS2: str = "iso.3.6.1.4.1.40297.1.2.1.2.10.0"
@@ -39,32 +44,140 @@ class SNMP:
     OID_WORK_STATUS: str = "iso.3.6.1.4.1.40297.1.2.4.12.0"
     OID_CUR_ZONE_ALIAS: str = "iso.3.6.1.4.1.40297.1.2.4.13.0"
 
+    READABLE_LABELS = {
+        OID_PSU_VOLTAGE: ("PSU Voltage", "%d mV"),
+        OID_PA_TEMPERATURE: ("PA Temperature", "%d m°C"),
+        OID_VSWR: ("VSWR", "%d dB"),
+        OID_TX_FWD_POWER: ("TX Forward Power", "%d mW"),
+        OID_TX_REF_POWER: ("TX Reflected Power", "%d mW"),
+        OID_RSSI_TS1: ("RSSI TS1", "%d dB"),
+        OID_RSSI_TS2: ("RSSI TS2", "%d dB"),
+        OID_REPEATER_MODEL: ("Repeater Model", "%s"),
+        OID_MODEL_NUMBER: ("Repeater Model Identification", "%s"),
+        OID_FIRMWARE_VERSION: ("Repeater Firmware", "%s"),
+        OID_RCDB_VERSION: ("Repeater Radio Data (RCDB)", "%s"),
+        OID_SERIAL_NUMBER: ("Repeater Serial No.", "%s"),
+        OID_RADIO_ALIAS: ("Radio Alias (Callsign)", "%s"),
+        OID_RADIO_ID: ("Repeater ID", "%d"),
+        OID_CUR_CHANNEL_NAME: ("Current Channel Name", "%s"),
+        OID_CUR_CHANNEL_MODE: (
+            "Current Channel Zone (0=DIGITAL, 1=ANALOG, 2=MIXED)",
+            "%d",
+        ),
+        OID_TX_FREQUENCE: ("TX Frequence", "%d Hz"),
+        OID_RX_FREQUENCE: ("RX Frequence", "%d Hz"),
+        OID_WORK_STATUS: ("Work Status (0=RECEIVE, 1=TRANSMIT)", "%d"),
+        OID_CUR_ZONE_ALIAS: ("Current Zone Alias", "%s"),
+    }
+
+    ALL_STRINGS = (
+        OID_REPEATER_MODEL,
+        OID_MODEL_NUMBER,
+        OID_FIRMWARE_VERSION,
+        OID_RCDB_VERSION,
+        OID_RADIO_ALIAS,
+        OID_CUR_ZONE_ALIAS,
+        OID_SERIAL_NUMBER,
+        OID_CUR_CHANNEL_NAME,
+    )
+
+    ALL_FLOATS = (
+        OID_PSU_VOLTAGE,
+        OID_VSWR,
+        OID_PA_TEMPERATURE,
+        OID_TX_FWD_POWER,
+        OID_TX_REF_POWER,
+    )
+
+    ALL_KNOWN = (
+        OID_PSU_VOLTAGE,
+        OID_PA_TEMPERATURE,
+        OID_VSWR,
+        OID_TX_FWD_POWER,
+        OID_TX_REF_POWER,
+        OID_RSSI_TS1,
+        OID_RSSI_TS2,
+        OID_REPEATER_MODEL,
+        OID_MODEL_NUMBER,
+        OID_FIRMWARE_VERSION,
+        OID_RCDB_VERSION,
+        OID_SERIAL_NUMBER,
+        OID_RADIO_ALIAS,
+        OID_RADIO_ID,
+        OID_CUR_CHANNEL_MODE,
+        OID_CUR_CHANNEL_NAME,
+        OID_TX_FREQUENCE,
+        OID_RX_FREQUENCE,
+        OID_WORK_STATUS,
+        OID_CUR_ZONE_ALIAS,
+    )
+
+    OID_WALK_BASE_1: str = "1.3.6.1.4.1.40297.1.2.4"
+    OID_WALK_BASE_2: str = "1.3.6.1.4.1.40297.1.2.1.2"
+
     @staticmethod
     def walk_ip(address: tuple, settings_storage: BridgeSettings) -> dict:
         ip, port = address
         try:
-            walk_result: list = snmp_walk(
-                "1.3.6.1.4.1.40297.1.2.4",
-                hostname=ip,
-                community=settings_storage.snmp_family,
-                version=1,
-            )
-            for snmp_var in walk_result:
-                if isinstance(snmp_var, SNMPVariable):
-                    value = snmp_var.value
-                    if snmp_var.oid in (
-                        SNMP.OID_REPEATER_MODEL,
-                        SNMP.OID_MODEL_NUMBER,
-                        SNMP.OID_FIRMWARE_VERSION,
-                        SNMP.OID_RCDB_VERSION,
-                        SNMP.OID_RADIO_ALIAS,
-                        SNMP.OID_CUR_ZONE_ALIAS,
-                        SNMP.OID_SERIAL_NUMBER,
-                        SNMP.OID_CUR_CHANNEL_NAME,
-                    ):
-                        value = octet_string_to_utf8(value)
-                    settings_storage.hytera_snmp_data[snmp_var.oid] = value
+            for oid in SNMP.ALL_KNOWN:
+                raw_oid = oid.replace("iso", "1")
+                snmp_result = get(
+                    ip=ip,
+                    community=settings_storage.snmp_family,
+                    oid=raw_oid,
+                    version=Version.V1,
+                )
+                if oid in SNMP.ALL_STRINGS:
+                    snmp_result = octet_string_to_utf8(str(snmp_result, "utf8"))
+                elif oid in SNMP.ALL_FLOATS:
+                    snmp_result = int.from_bytes(snmp_result, byteorder="big")
+                settings_storage.hytera_snmp_data[oid] = snmp_result
+
         except SystemError:
             pass
 
+        SNMP.print_snmp_data(settings_storage=settings_storage)
+
         return settings_storage.hytera_snmp_data
+
+    @staticmethod
+    def print_snmp_data(settings_storage: BridgeSettings):
+        longest_label = 0
+        for key in SNMP.READABLE_LABELS:
+            label_len = len(SNMP.READABLE_LABELS.get(key)[0])
+            if label_len > longest_label:
+                longest_label = label_len
+
+        for key in settings_storage.hytera_snmp_data:
+            print_settings = SNMP.READABLE_LABELS.get(key)
+            if print_settings:
+                value = settings_storage.hytera_snmp_data.get(key)
+                print(
+                    "%s| %s"
+                    % (
+                        str(print_settings[0]).ljust(longest_label + 5),
+                        print_settings[1] % value,
+                    )
+                )
+
+
+if __name__ == "__main__":
+    if len(sys.argv) < 2:
+        print("use as snmp.py <ip of hytera repeater>")
+        exit(1)
+
+    minimal_settings: str = """
+    [ip-site-connect]
+    ip = 192.168.1.2
+    p2p_port = 50000
+    dmr_port = 50001
+    rdac_port = 50002
+
+    [homebrew]
+    local_ip = 0.0.0.0
+    master_ip = 192.168.1.3
+    master_port = 62031
+    password = B3S3CURE
+    """
+    settings: BridgeSettings = BridgeSettings(filedata=minimal_settings)
+    SNMP().walk_ip((sys.argv[1], 0), settings_storage=settings)
