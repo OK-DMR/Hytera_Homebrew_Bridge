43c64da781f368a23fc25f97067207dd426b8a9c
Marek Sebera <marek.sebera@gmail.com>
2020-12-25 20:06:44 +0100

    Tag version 2020.3

--- setup.py
+++ setup.py
@@ -17,7 +17,7 @@ setup(
     author="Marek Sebera",
     author_email="marek.sebera@gmail.com",
     license="AGPL-3.0",
-    version="2020.2",
+    version="2020.3",
     packages=[
         "hytera_homebrew_bridge",
         "hytera_homebrew_bridge.kaitai",
