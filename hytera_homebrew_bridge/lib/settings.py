6dc684c88eabc84c17708fd00f059ef1f761bf48
Marek Sebera <marek.sebera@gmail.com>
2021-01-01 11:34:35 +0100

    Tag version 2021.1

--- hytera_homebrew_bridge/lib/settings.py
+++ hytera_homebrew_bridge/lib/settings.py
@@ -82,7 +82,7 @@ class BridgeSettings(LoggingTrait):
             fallback="http://github.com/ok-dmr/Hytera_Homebrew_Bridge",
         )
         self.hb_software_id = parser.get(
-            self.SECTION_HB, "software_id", fallback="2020.4"
+            self.SECTION_HB, "software_id", fallback="2021.1"
         )
         self.hb_package_id = parser.get(
             self.SECTION_HB, "package_id", fallback="Hytera Homebrew Bridge"
